4d3ef9ad393a238f02cb70672159dfaf450e97a9
Dalek-Sec <Dalek-Sec@users.noreply.github.com>
2019-05-08 22:03:41 -0500

    Refactor into library and cli, add docstrings

---
+++ src/bin/vish.rs
@@ -0,0 +1,14 @@
+use std::env;
+
+use visualhash::*;
+
+fn main() {
+    let key = env::args()
+        .last()
+        .expect("Missing argument to use as data for image");
+    println!("Visualizing '{}'", &key);
+    let key: &[u8] = key.as_bytes();
+    let img = visualize(key);
+    img.save("visual.png").unwrap();
+    println!("Saved as visual.png")
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,36 @@
 extern crate argon2;
 extern crate image;
 
-fn main() {
-    println!("Hello, world!");
-    let hash = hash(b"sup");
-    println!("{:?}", hash);
-    let image = generate_image(&hash);
-    image.save("test.png").unwrap();
+/// Convert some bytes into a (hopefully) unique looking image  
+/// Can easily be saved to a file with `.save()`
+pub fn visualize(key: &[u8]) -> image::RgbImage {
+    let hash = hash(key);
+    let img = generate_image(&hash);
+
+    img
 }
 
+/// Use KDF (to increase expense to find keys with valid look-alike images)
+/// with consistent parameters (to be deterministic)
 fn hash(key: &[u8]) -> Vec<u8> {
     let salt: &[u8] = &[0; 8];
     let mut config = argon2::Config::default();
     config.hash_length = 128;
     let hash = argon2::hash_raw(key, salt, &config).unwrap();
-    
+
     hash
 }
 
+/// For each RGB value for each pixel in the image, use a byte from the hash
 fn generate_image(data: &[u8]) -> image::RgbImage {
     let mut img: image::RgbImage = image::ImageBuffer::new(6, 6);
     let mut data_iter = data.iter();
-    for (x, y, pixel) in img.enumerate_pixels_mut() {
+    for (_x, _y, pixel) in img.enumerate_pixels_mut() {
         let r = data_iter.next().unwrap();
-        println!("{:?}", r);
         let g = data_iter.next().unwrap();
-        println!("{:?}", g);
         let b = data_iter.next().unwrap();
-        println!("{:?}", b);
         *pixel = image::Rgb([*r, *g, *b]);
     }
-    println!("{:?}", img);
 
     img
 }
-
